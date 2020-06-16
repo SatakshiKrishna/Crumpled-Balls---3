dcf50329a62dc5f9225a8835bcf64c8776e3c597
SatakshiKrishna <65105346+SatakshiKrishna@users.noreply.github.com>
2020-06-16 15:12:33 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,27 @@
+class Ball {
+  constructor(x, y, r) {
+    var options = {
+      restitution:0.3,
+      density:1.3,
+      friction:0.5,
+       isStatic:false
+    }
+    this.body = Bodies.circle(x, y,r, options);
+    this.radius = r;
+   
+    this.image = loadImage("Paper.png");
+
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+   
+    push();
+    translate(pos.x, pos.y);
+   
+    imageMode(CENTER);
+    
+    image(this.image,0,0, this.radius);
+    pop();
+  }
+};
